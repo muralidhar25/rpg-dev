244df7ac0b483611e9bdf4cb38ba1d70377208dd
pratima <pratimaducktaleit@gmail.com>
2019-07-10 19:07:15 +0530

    #716 merge conflict

--- RPGSmithApp/RPGSmithApp/ClientApp/src/app/core/services/combat.service.ts
+++ RPGSmithApp/RPGSmithApp/ClientApp/src/app/core/services/combat.service.ts
@@ -7,12 +7,13 @@ import 'rxjs/add/operator/map';
 import { EndpointFactory } from '../common/endpoint-factory.service';
 import { ConfigurationService } from '../common/configuration.service';
 import { CombatSettings } from '../models/view-models/combatSettings.model';
+import { initiative } from '../models/view-models/initiative.model';
 
 @Injectable()
 export class CombatService extends EndpointFactory {
   private readonly GetCombatDetails: string = this.configurations.baseUrl + "/api/Combat/GetCombatDetails";
   private readonly UpdateCombatSettings: string = this.configurations.baseUrl + "/api/Combat/UpdateCombatSettings";
-  private readonly GetInitiativeDetails: string = this.configurations.baseUrl + "/api/Combat/GetInitiativeDetails";
+  private readonly SaveCombatantList: string = this.configurations.baseUrl + "/api/Combat/SaveCombatantList";
 
   constructor(http: HttpClient, configurations: ConfigurationService, injector: Injector) {
     super(http, configurations, injector);
@@ -28,22 +29,21 @@ export class CombatService extends EndpointFactory {
   }
 
   updateCombatSettings<T>(settings: CombatSettings): Observable<T> {
-    let updateUrl = `${this.UpdateCombatSettings}`
-    return this.http.post<T>(updateUrl, JSON.stringify(settings), this.getRequestHeaders())
+    let updateCombatSettingsUrl = `${this.UpdateCombatSettings}`
+    return this.http.post<T>(updateCombatSettingsUrl, JSON.stringify(settings), this.getRequestHeaders())
       .catch(error => {
         return this.handleError(error, () => this.updateCombatSettings(settings));
       });
   }
 
-  //getInitiativeDetails<T>(CampaignID: number): Observable<T> {
-  //  debugger;
-  //  let endpointUrl = `${this.GetInitiativeDetails}?CampaignId=${CampaignID}`;
-
-  //  return this.http.get<T>(endpointUrl, this.getRequestHeaders())
-  //    .catch(error => {
-  //      return this.handleError(error, () => this.getInitiativeDetails(CampaignID));
-  //    });
-  //}
+  saveCombatantList<T>(combatants: any[]): Observable<T> {
+    debugger;
+    let saveInitiativeUrl = `${this.SaveCombatantList}`
+    return this.http.post<T>(saveInitiativeUrl, JSON.stringify(combatants), this.getRequestHeaders())
+      .catch(error => {
+        return this.handleError(error, () => this.saveCombatantList(combatants));
+      });
+  }
 
 
 }
