b17350010c40e2cdc89ff601d8827861cefd9aaf
Naren <narender07071949@gmail.com>
2019-07-11 16:45:09 +0530

    Last Command for Ruleset

---
+++ RPGSmithApp/RPGSmithApp/Migrations/20190711100924_LastCommandRuleSet.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RPGSmithApp.Migrations
+{
+    public partial class LastCommandRuleSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastCommand",
+                table: "RuleSets",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastCommandResult",
+                table: "RuleSets",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastCommandTotal",
+                table: "RuleSets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastCommandValues",
+                table: "RuleSets",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastCommand",
+                table: "RuleSets");
+
+            migrationBuilder.DropColumn(
+                name: "LastCommandResult",
+                table: "RuleSets");
+
+            migrationBuilder.DropColumn(
+                name: "LastCommandTotal",
+                table: "RuleSets");
+
+            migrationBuilder.DropColumn(
+                name: "LastCommandValues",
+                table: "RuleSets");
+        }
+    }
+}
