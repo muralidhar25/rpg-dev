c2a956a4ccb4ca1ee19926e25b08a7079369b684
pratima <pratimaducktaleit@gmail.com>
2020-01-14 16:01:35 +0530

    Bug fixes

--- RPGSmithApp/RPGSmithApp/ClientApp/src/app/records/loot/giveaway/giveaway.component.ts
+++ RPGSmithApp/RPGSmithApp/ClientApp/src/app/records/loot/giveaway/giveaway.component.ts
@@ -54,6 +54,9 @@ export class GiveawayComponent implements OnInit {
       try { currencyTypesList.forEach((x, i) => { x.lootId = 0; }); } catch (err) { }
 
       this.giveLootCurrency = currencyTypesList;
+      this.giveLootCurrency.map(x => {
+        x.amount = x.amount ? x.amount : 0;
+      });      
 
       this.initialize();
     }, 0);
